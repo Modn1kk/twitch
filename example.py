798dd87d4ec0f4bc6529e7e07bb398462f883379
Mikhail <modn1kcs@gmail.com>
2023-10-01 13:17:13 +0300

    release

---
+++ example.py
@@ -0,0 +1,4 @@
+from TwitchChannelPointsMiner import TwitchChannelPointsMiner
+from TwitchChannelPointsMiner.classes.Settings import FollowersOrder
+twitch_miner = TwitchChannelPointsMiner("modn1kcsgo")
+twitch_miner.mine(["tokyoto99", "uglyfacekid", "dasha228play", "fumimochi", "m1sssst", "leskakilka"])
